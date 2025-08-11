27051b147e8662da99f9cafbdc21559151b3e41c
programming666 <code666@qinyining.cn>
2025-08-11 12:14:08 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3002',
+        target: 'https://backmodel-recommendation.qinyining.cn',
         changeOrigin: true
       }
     }
